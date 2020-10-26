0f4a96374d720f487e735db62607e1a04c875bac
Mohammad Hasnain <hasnain2808@gmail.com>
2020-10-26 16:23:45 +0530

    feat(Bank Reconcilation): Add bank statement

---
+++ erpnext/accounts/doctype/bank_statement/bank_statement.js
@@ -0,0 +1,25 @@
+// Copyright (c) 2020, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Bank Statement', {
+	// refresh: function(frm) {
+
+	// }
+	company: function(frm){
+		set_bank_account_filter(frm)
+	},
+	bank:function(frm) {
+		set_bank_account_filter(frm)
+	}
+});
+
+function set_bank_account_filter(frm){
+	frm.set_query("bank_account", function () {
+		return {
+			"filters": {
+				"company": frm.doc.company ,
+				"bank": frm.doc.bank
+			}
+		};
+	});
+}
