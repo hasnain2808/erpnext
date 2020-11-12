c70f86302a203d8399698836e6244d0f0c9923c7
Mohammad Hasnain <hasnain2808@gmail.com>
2020-11-12 16:46:06 +0530

    Refactor(Bank Reconciliation): segregate js

---
+++ erpnext/accounts/page/bank_reconciliation_tool/import_preview_dialog.js
@@ -0,0 +1,209 @@
+frappe.provide("erpnext.accounts");
+
+frappe.require('/assets/js/data_import_tools.min.js', () => {
+
+	erpnext.accounts.ImportPreviewDialog = class ImportPreviewDialog extends frappe.data_import.ImportPreview {
+		constructor(opts) {
+			super(opts);
+			// this.show();
+		}
+
+		add_actions() {
+			let actions = [
+				{
+					label: __('Map Columns'),
+					handler: 'show_column_mapper',
+					condition: true//this.frm.doc.status !== 'Success'
+				},
+				{
+					label: __('Export Errored Rows'),
+					handler: 'export_errored_rows',
+					condition: this.import_log.filter(log => !log.success).length > 0
+				},
+				// {
+				// 	label: __('Show Warnings'),
+				// 	handler: 'show_warnings',
+				// 	condition: this.preview_data.warnings.length > 0
+				// }
+			];
+	
+			let html = actions
+				.filter(action => action.condition)
+				.map(action => {
+					return `<button class="btn btn-sm btn-default" data-action="${action.handler}">
+						${action.label}
+					</button>
+				`;
+				});
+	
+			this.wrapper.find('.table-actions').html(html);
+		}
+
+
+		show_column_mapper() {
+			let column_picker_fields = this.get_columns_for_picker(this.doctype);
+			let changed = [];
+			let fields = this.preview_data.columns.map((col, i) => {
+				let df = col.df;
+				if (col.header_title === 'Sr. No') return [];
+
+				let fieldname;
+				if (!df) {
+					fieldname = null;
+				} else if (col.map_to_field) {
+					fieldname = col.map_to_field;
+				} else if (col.is_child_table_field) {
+					fieldname = `${col.child_table_df.fieldname}.${df.fieldname}`;
+				} else {
+					fieldname = df.fieldname;
+				}
+				return [
+					{
+						label: '',
+						fieldtype: 'Data',
+						default: col.header_title,
+						fieldname: `Column ${i}`,
+						read_only: 1
+					},
+					{
+						fieldtype: 'Column Break'
+					},
+					{
+						fieldtype: 'Autocomplete',
+						fieldname: i,
+						label: '',
+						max_items: Infinity,
+						options: [
+							{
+								label: __("Don't Import"),
+								value: "Don't Import"
+							}
+						].concat(this.get_fields_as_options(this.doctype, column_picker_fields)),
+						default: fieldname || "Don't Import",
+						change() {
+							changed.push(i);
+						}
+					},
+					{
+						fieldtype: 'Section Break'
+					}
+				];
+			});
+			// flatten the array
+			fields = fields.reduce((acc, curr) => [...acc, ...curr]);
+			let file_name = (this.frm.get_value('import_file') || '').split('/').pop();
+			let parts = [file_name.bold(), this.doctype.bold()];
+			fields = [
+				{
+					fieldtype: 'HTML',
+					fieldname: 'heading',
+					options: `
+						<div class="margin-top text-muted">
+						${__('Map columns from {0} to fields in {1}', parts)}
+						</div>
+					`
+				},
+				{
+					fieldtype: 'Section Break'
+				}
+			].concat(fields);
+
+			let dialog = new frappe.ui.Dialog({
+				title: __('Map Columns'),
+				fields,
+				primary_action: values => {
+					let changed_map = {};
+					changed.map(i => {
+						let header_row_index = i - 1;
+						changed_map[header_row_index] = values[i];
+					});
+					if (changed.length > 0) {
+						this.events.remap_column(changed_map);
+					}
+					dialog.hide();
+				}
+			});
+			dialog.$body.addClass('map-columns');
+			dialog.show();
+		}
+
+
+		get_columns_for_picker(doctype) {
+			let out = {};
+
+			const exportable_fields = df => {
+				let keep = true;
+				if (frappe.model.no_value_type.includes(df.fieldtype)) {
+					keep = false;
+				}
+				if (['lft', 'rgt'].includes(df.fieldname)) {
+					keep = false;
+				}
+				return keep;
+			};
+		
+			// parent
+			let doctype_fields = frappe.meta
+				.get_docfields(doctype)
+				.filter(exportable_fields);
+		
+			out[doctype] = [
+				{
+					label: __('ID'),
+					fieldname: 'name',
+					fieldtype: 'Data',
+					reqd: 1
+				}
+			].concat(doctype_fields);
+		
+			// children
+			const table_fields = frappe.meta.get_table_fields(doctype);
+			table_fields.forEach(df => {
+				const cdt = df.options;
+				const child_table_fields = frappe.meta
+					.get_docfields(cdt)
+					.filter(exportable_fields);
+		
+				out[df.fieldname] = [
+					{
+						label: __('ID'),
+						fieldname: 'name',
+						fieldtype: 'Data',
+						reqd: 1
+					}
+				].concat(child_table_fields);
+			});
+		
+			return out;
+		}
+		
+
+		get_fields_as_options(doctype, column_map) {
+			let keys = [doctype];
+			frappe.meta.get_table_fields(doctype).forEach(df => {
+				keys.push(df.fieldname);
+			});
+			// flatten array
+			return [].concat(
+				...keys.map(key => {
+					return column_map[key].map(df => {
+						let label = df.label;
+						let value = df.fieldname;
+						if (doctype !== key) {
+							let table_field = frappe.meta.get_docfield(doctype, key);
+							label = `${df.label} (${table_field.label})`;
+							value = `${table_field.fieldname}.${df.fieldname}`;
+						}
+						return {
+							label,
+							value,
+							description: value
+						};
+					});
+				})
+			);
+		}
+
+
+	}
+})
---
+++ erpnext/accounts/page/bank_reconciliation_tool/upload_statement.js
@@ -0,0 +1,318 @@
+frappe.provide("erpnext.accounts");
+
+erpnext.accounts.UploadStatememt = class UploadStatememt {
+	constructor(wrapper) {
+		this.make_upload_statement_dialog()
+		// return this.upload_statement_dialog;
+    }
+
+
+	make_upload_statement_dialog(){
+		const me = this;
+		const fields = [
+			{
+				fieldtype: "Link",
+				fieldname: "company",
+				label: __("Company"),
+				options: "Company",
+				change: () => {
+					// this.upload_statement_dialog.get_field("import_bank_transactions").df.hidden=0;
+					// this.upload_statement_dialog.get_field("import_bank_transactions").refresh();
+					// console.log(t)
+					// if (!this.upload_statement_dialog.get_value("company"))
+					// $("[data-fieldname=upload_statement_dialog]").hide()
+					// 		else
+					// 		$("[data-fieldname=upload_statement_dialog]").show()
+					me.upload_statement_dialog.refresh_dependency()
+				}
+			},
+
+			{
+				fieldtype: "Link",
+				fieldname: "bank_account",
+				label: __("Bank Account"),
+				options: "Bank Account",
+				depends_on: "eval: doc.company == 'moha' ",
+				get_query: () => {
+					return {
+						filters: {
+							company: [
+								"in",
+								[this.upload_statement_dialog.get_value("company") || ""],
+							],
+						},
+					};
+				},
+				change: ()=>{
+					me.upload_statement_dialog.get_field("bank_account").validate_link_and_fetch()
+
+					// me.upload_statement_dialog.get_field("bank").refresh()
+				}
+
+			},
+			{
+				fieldtype: "Link",
+				fieldname: "bank",
+				label: __("Bank"),
+				options: "Bank",
+				depends_on: "eval: doc.company == 'moha' ",
+				fetch_from:"doc.bank_account.bank",
+				// get_query: () => {
+				// 	return {
+				// 		filters: {
+				// 			company: [
+				// 				"in",
+				// 				[this.upload_statement_dialog.get_value("company") || ""],
+				// 			],
+				// 		},
+				// 	};
+				// },
+
+			},
+			{
+				fieldtype: "Column Break",
+			},
+			{
+				fieldtype: "Attach",
+				fieldname: "import_bank_transactions",
+				label: __("Import Bank Transactions"),
+				change: () => {
+					this.show_preview()
+				}
+				// get_query: () => {
+				// 	return {
+				// 		filters: {
+				// 			company: [
+				// 				"in",
+				// 				[this.upload_statement_dialog.get_value("company") || ""],
+				// 			],
+				// 		},
+				// 	};
+				// }
+			},
+			{
+				fieldname: "import_warnings_section",
+				fieldtype: "Section Break",
+				label: "Import File Errors and Warnings"
+			   },
+			   {
+				fieldname: "import_warnings",
+				fieldtype: "HTML",
+				label: "Import Warnings"
+			   },
+			   {
+				fieldname: "section_import_preview",
+				fieldtype: "Section Break",
+				label: "Preview"
+			   },
+			{
+				fieldname: "import_preview",
+				fieldtype: "HTML",
+				label: "Import Preview"
+			   },
+			   {
+				fieldname: "template_warnings",
+				fieldtype: "Code",
+				hidden: 1,
+				label: "Template Warnings",
+				options: "JSON"
+			   },
+
+			//    {
+			// 	fieldname: "template_options",
+			// 	fieldtype: "Code",
+			// 	hidden: 1,
+			// 	label: "Template Options",
+			// 	options: "JSON",
+			// 	read_only: 1
+			//    },
+			   {
+				fieldname: "data_import_id",
+				fieldtype: "Data",
+				hidden: 1,
+				label: "Data Import ID",
+				read_only: 1
+			   },
+
+		]
+
+		this.upload_statement_dialog = new frappe.ui.Dialog({
+			title: __("Upload Bank statements"),
+			fields: fields,
+			size: "large",
+			primary_action: values => {
+					frappe
+						.call({
+							method: 'erpnext.accounts.page.bank_reconciliation_tool.bank_reconciliation_tool.start_import',
+							args: {
+								data_import:  this.upload_statement_dialog.get_value("data_import_id")
+							},
+							// btn: frm.page.btn_primary
+						})
+						// .then(r => {
+						// 	if (r.message === true) {
+						// 		frm.disable_save();
+						// 	}
+						// });
+						this.upload_statement_dialog.hide();
+						delete this.upload_statement_dialog;
+						this.make_upload_statement_dialog();
+			}
+		});
+	}
+
+	show_preview(){
+		const me = this;
+		const file_name = me.upload_statement_dialog.get_value("import_bank_transactions")
+		const data_import_id = me.upload_statement_dialog.get_value("data_import_id")
+		// frm.toggle_display('section_import_preview', frm.has_import_file());
+		// if (!frm.has_import_file()) {
+		// 	frm.get_field('import_preview').$wrapper.empty();
+		// 	return;
+		// } else {
+		// 	frm.trigger('update_primary_action');
+		// }
+
+		// load import preview
+		this.upload_statement_dialog.get_field('import_preview').$wrapper.empty();
+		$('<span class="text-muted">')
+			.html(__('Loading import file...'))
+			.appendTo(this.upload_statement_dialog.get_field('import_preview').$wrapper);
+
+
+
+		frappe.call({
+				method: 'erpnext.accounts.page.bank_reconciliation_tool.bank_reconciliation_tool.get_importer_preview',
+				args: {
+					import_file_path: file_name,
+					data_import: data_import_id ,
+					template_options: me.template_options
+				},
+				error: {
+					TimestampMismatchError() {
+						// ignore this error
+					}
+				}
+			})
+			.then(r => {
+				let preview_data = r.message["preview"];
+				me.upload_statement_dialog.set_value('data_import_id', r.message["import_name"]);
+				me.show_import_preview(me.upload_statement_dialog, preview_data)
+				me.show_import_warnings(me.upload_statement_dialog, preview_data)
+			});
+	}
+
+	show_import_preview(frm, preview_data) {
+		const me = this;
+		let import_log = JSON.parse(frm.get_value('import_log') || '[]');
+;
+		me.upload_statement_dialog.get_primary_btn().show()
+
+		if (
+			frm.import_preview &&
+			frm.import_preview.doctype === frm.get_value('reference_doctype')
+		) {
+			frm.import_preview.preview_data = preview_data;
+			frm.import_preview.import_log = import_log;
+			frm.import_preview.refresh();
+			return;
+		}
+		// frappe.require('/assets/js/data_import_tools.min.js', () => {
+			frm.import_preview = new erpnext.accounts.ImportPreviewDialog({
+				wrapper: frm.get_field('import_preview').$wrapper,
+				doctype: "Bank Transaction",
+				preview_data,
+				import_log,
+				frm,
+				events: {
+					remap_column(changed_map) {
+						console.log(changed_map)
+						let template_options = JSON.parse(me.template_options || '{}');
+						console.log(frm.get_value('template_options'))
+						template_options.column_to_field_map = template_options.column_to_field_map || {};
+						Object.assign(template_options.column_to_field_map, changed_map);
+						console.log(template_options.column_to_field_map)
+						me.template_options = JSON.stringify(template_options);
+						console.log(me.template_options)
+						console.log(me.template_options)
+						me.show_preview()
+					}
+				}
+			});
+		// });
+	}
+	show_import_warnings(frm, preview_data) {
+		let columns = preview_data.columns;
+		let warnings = JSON.parse(frm.get_value('template_warnings') || '[]');
+		warnings = warnings.concat(preview_data.warnings || []);
+
+		// frm.toggle_display('import_warnings_section', warnings.length > 0);
+		// if (warnings.length>0){
+		// 	frm.get_field("import_warnings_section").$wrapper.hide()
+		// }
+		if (warnings.length === 0) {
+			frm.get_field('import_warnings').$wrapper.html('');
+			return;
+		}
+
+		// group warnings by row
+		let warnings_by_row = {};
+		let other_warnings = [];
+		for (let warning of warnings) {
+			if (warning.row) {
+				warnings_by_row[warning.row] = warnings_by_row[warning.row] || [];
+				warnings_by_row[warning.row].push(warning);
+			} else {
+				other_warnings.push(warning);
+			}
+		}
+
+		let html = '';
+		html += Object.keys(warnings_by_row)
+			.map(row_number => {
+				let message = warnings_by_row[row_number]
+					.map(w => {
+						if (w.field) {
+							let label =
+								w.field.label +
+								(w.field.parent !== frm.doc.reference_doctype
+									? ` (${w.field.parent})`
+									: '');
+							return `<li>${label}: ${w.message}</li>`;
+						}
+						return `<li>${w.message}</li>`;
+					})
+					.join('');
+				return `
+				<div class="warning" data-row="${row_number}">
+					<h5 class="text-uppercase">${__('Row {0}', [row_number])}</h5>
+					<div class="body"><ul>${message}</ul></div>
+				</div>
+			`;
+			})
+			.join('');
+
+		html += other_warnings
+			.map(warning => {
+				let header = '';
+				if (warning.col) {
+					let column_number = `<span class="text-uppercase">${__('Column {0}', [warning.col])}</span>`;
+					let column_header = columns[warning.col].header_title;
+					header = `${column_number} (${column_header})`;
+				}
+				return `
+					<div class="warning" data-col="${warning.col}">
+						<h5>${header}</h5>
+						<div class="body">${warning.message}</div>
+					</div>
+				`;
+			})
+			.join('');
+		frm.get_field('import_warnings').$wrapper.html(`
+			<div class="row">
+				<div class="col-sm-10 warnings">${html}</div>
+			</div>
+		`);
+	}
+
+}
